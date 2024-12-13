1e9b26c0f286756d5a9f538026139d072be31f7a
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2024-12-13 13:55:52 +0100

    añadir estilos + javascript en repasoGeneral;

---
+++ repasoGeneral/main.js
@@ -0,0 +1,57 @@
+const botonAgregar = document.getElementById("agregarTarea");
+console.log("botonAgregar:", botonAgregar);
+
+
+function agregarTarea() {
+    //seleccionar el elemento con el identificador "listaTareas"
+    let listaTareas = document.getElementById("listaTareas");
+    //declarar una variable que tenga como valor la creación de un
+    //elemento div
+    let tarea = document.createElement("div");
+    //pintar por consola los dos elementos
+    //console.log("listaTareas:", listaTareas, "nuevo elemento:", tarea);
+    //tarea.textContent = "Nueva tarea";
+
+    //Asignar a una nueva variable textoTarea el valor que introduzca el usuario por la ventana
+    let textoTarea = prompt("Introduce el nombre de la tarea");
+
+    //Asignamos el valor introducido por ventana al textcontent de la tarea
+    tarea.textContent = textoTarea;
+
+    //Asignamos una clase llamada "tarea" al elemento tarea
+    tarea.className = "tarea";
+
+    //creamos un nuevo elemento "button"
+    let botonEliminar = document.createElement("button");
+
+    //Asignamos el textContent de la nueva variable botonEliminar
+    botonEliminar.textContent = "Eliminar";
+
+    //Le asignamos una clase también
+    botonEliminar.className = "eliminar";
+
+    //Añadimos como elemento hijo la variable botonEliminar a la variable tarea 
+    tarea.appendChild(botonEliminar);
+
+    // Asignamos en el click la invocación de la función eliminarTarea pasandole por parametro el propio botonEliminar
+    botonEliminar.onclick = function () {
+        eliminarTarea(this);
+    }
+
+
+    //Añadimos como elemento hijo la variable tarea al elemento con el identificador "listaTareas" 
+    listaTareas.appendChild(tarea);
+}
+
+
+botonAgregar.onclick = function () {
+    agregarTarea();
+}
+
+//Declarar una nueva función llamada eliminarTarea
+// boton por parámetro
+// recuperar el elemento padre de la variable boton y eliminarlo
+function eliminarTarea (boton) {
+    let elementoPadre = boton.parentElement;
+    elementoPadre.remove();
+}
