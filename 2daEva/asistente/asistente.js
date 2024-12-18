bb71ad75dd390e1ba7ac3951169445115462810b
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2024-12-18 09:59:34 +0100

    añadir intrucciones para la opción 2;

--- 2daEva/asistente/asistente.js
+++ 2daEva/asistente/asistente.js
@@ -8,13 +8,13 @@ let anioActual = new Date().getFullYear();
 
 //console.log("Tu edad es", edad);
 
-let opcion = prompt("¿Qué opción quieres elegir? 1 o 2");
+let opcion = prompt("¿Qué opción quieres elegir? 1 (número random ) o 2 (operaciones)");
 console.log("Opción elegida", opcion);
 
 if (opcion === "1") {
     //alert("Has elegido la opcion" + opcion);
 
-    let numeroRandom = Math.floor(Math.random() * 10);
+    let numeroRandom = Math.floor(Math.random() * 10) + 1;
     let numeroUsuario = Number(prompt("Introduce un nº de 1 al 10"));
 
     console.log("El nº random es", numeroRandom, " y el nº del usuario es:", numeroUsuario);
@@ -26,7 +26,7 @@ if (opcion === "1") {
             break;
         }
         else {
-            numeroUsuario = Number(prompt("Introduce un nº de 0 al 10"));
+            numeroUsuario = Number(prompt("Introduce un nº de 1 al 10"));
         }
 
         if (intentos === 0) {
@@ -38,7 +38,26 @@ if (opcion === "1") {
 
 }
 else if (opcion === "2") {
-    alert("Has elegido la opcion" + opcion);
+    // alert("Has elegido la opcion" + opcion);
+    let num1 = prompt("Introduce el primer número.");
+    console.log("1er número: ", num1);
+
+    let num2 = prompt("Introduce el segundo número.");
+    console.log("2do número: ", num2);
+    
+
+    let operacion = prompt("¿Qué operación deseas realizar (+, -, *, /)?");
+    console.log("operación usuario:", operacion);
+
+    let resultado = 0;
+
+    if (operacion === "+") resultado = Number(num1) + Number(num2);
+    if (operacion === "-") resultado = Number(num1) - Number(num2);
+    if (operacion === "*") resultado = Number(num1) * Number(num2);
+    if (operacion === "/") resultado = Number(num1) / Number(num2);
+
+    console.log("resultado de", operacion, " de los dos números: ", resultado);
+    
 }
 
 
