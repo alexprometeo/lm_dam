b7be8a0f10ee9ec1cffd17ea8928fc2576a2d6fe
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2025-04-25 14:21:24 +0200

    JSONB - Insertar registros con valores JSON;

---
+++ 3era_Eva/LenguajeMarcas/JSONB/insert_usuarios.sql
@@ -0,0 +1,33 @@
+
+INSERT INTO usuarios(nombre, caracteristicas)
+VALUES(
+	'Ana'
+	, '{
+		"idioma": "es",
+		"tema": "oscuro",
+		"notificaciones_activas": true
+	}'
+
+);
+
+INSERT INTO usuarios(nombre, caracteristicas)
+VALUES(
+	'Juan'
+	, '{
+		"idioma": "es",
+		"tema": "oscuro",
+		"notificaciones_activas": true
+	}'
+
+);
+
+INSERT INTO usuarios(nombre, caracteristicas)
+VALUES(
+	'Miquel'
+	, '{
+		"idioma": "cat",
+		"tema": "claro",
+		"notificaciones_activas": false
+	}'
+
+);
