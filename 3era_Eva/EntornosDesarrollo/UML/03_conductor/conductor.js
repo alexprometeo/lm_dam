1f3bad35ad198d8b694fd923d9267d0005808e80
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2025-05-08 13:48:51 +0200

    ED - UML - Conductor;

---
+++ 3era_Eva/EntornosDesarrollo/UML/03_conductor/conductor.js
@@ -0,0 +1,34 @@
+class Persona {
+    constructor(nombre, pais, edad) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.edad = edad;
+    }
+
+    mostrarDetalles() {
+        console.log(`${this.nombre} - ${this.pais} - ${this.edad}`);
+    }
+}
+
+class Conductor extends Persona {
+    constructor(nombre, pais, edad, licencia) {
+        super(nombre, pais, edad);
+        this.licencia = licencia;
+    }
+
+    mostrarDetalles() {
+        console.log(`${this.nombre} - ${this.pais} - ${this.edad} - ${this.licencia}`);
+    }
+}
+
+class Vehiculo {
+    constructor(marca, modelo, año) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+    }
+
+    mostrarDetalles() {
+        console.log(`${this.marca} - ${this.modelo} - ${this.año}`);
+    }
+}
