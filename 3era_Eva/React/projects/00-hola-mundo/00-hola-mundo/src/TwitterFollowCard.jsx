6915b4b71b7c96567a72334c0d27b8cf72ad6025
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2025-05-23 13:51:27 +0200

    React - Asignar clases y aplicarle estilos al componente;

--- 3era_Eva/React/projects/00-hola-mundo/00-hola-mundo/src/TwitterFollowCard.jsx
+++ 3era_Eva/React/projects/00-hola-mundo/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -2,13 +2,15 @@ import './TwitterFollowCard.css'
 
 export function TwitterFollowCard() {
     return (
-        <article>
-            <img alt="User Avatar" src="https://unavatar.io/user"/>
-            <section>
-                <h1>User</h1>
-                <em>@username</em>
-            </section>
-            <button>Seguir</button>
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar' alt="User Avatar" src="https://unavatar.io/user"/>
+                <section className='tw-followCard-info'>
+                    <h1>User</h1>
+                    <em className='tw-followCard-infoUserName'>@username</em>
+                </section>
+            </header>
+            <button className='tw-followCard-button'>Seguir</button>
         </article>
     )
 }
