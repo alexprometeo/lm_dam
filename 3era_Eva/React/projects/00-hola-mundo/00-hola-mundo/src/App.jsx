19df608d37a8373246b39d25d8e3316a0a8b31a6
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2025-05-28 10:06:59 +0200

    React - Componente de prueba y props de tipo booleano y funciones;

--- 3era_Eva/React/projects/00-hola-mundo/00-hola-mundo/src/App.jsx
+++ 3era_Eva/React/projects/00-hola-mundo/00-hola-mundo/src/App.jsx
@@ -1,11 +1,14 @@
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 function App() {
+
+  // const format = (userName) => {return `@${userName}`}
+
   return (
     <>
-      <TwitterFollowCard name="José" userName="xX_jose_Xx"/>
+      <TwitterFollowCard name="José" userName="xX_jose_Xx" isFollowing={true}/>
       <TwitterFollowCard/>
-      <TwitterFollowCard name="Diane" userName="dianeee"/>
+      <TwitterFollowCard name="Diane" userName="dianeee" isFollowing={true}/>
     </>
   )
 }
--- 3era_Eva/React/projects/00-hola-mundo/00-hola-mundo/src/TwitterFollowCard.jsx
+++ 3era_Eva/React/projects/00-hola-mundo/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,8 +1,11 @@
 import './TwitterFollowCard.css'
 
-export function TwitterFollowCard({name = "Unknown", userName = "unknown"}) {
+// export function TwitterFollowCard({name = "Unknown", userName = "unknown", isFollowing = false, formatUserName}) {
+export function TwitterFollowCard({name = "Unknown", userName = "unknown", isFollowing = false}) {
     const avatarSource = `https://unavatar.io/${userName}`;
     const avatarAlt = `${name} Avatar`;
+
+    console.log(`El usuario ${userName} tiene isFollowing con este valor: ${isFollowing}`);
 
     return (
         <article className='tw-followCard'>
@@ -11,6 +14,7 @@ export function TwitterFollowCard({name = "Unknown", userName = "unknown"}) {
                 <section className='tw-followCard-info'>
                     <h1>{name}</h1>
                     <em className='tw-followCard-infoUserName'>@{userName}</em>
+                    {/* <em className='tw-followCard-infoUserName'>{formatUserName}</em> */}
                 </section>
             </header>
             <button className='tw-followCard-button'>Seguir</button>
